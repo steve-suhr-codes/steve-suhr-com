0f763cf04ef094d22f72eb4aaf6f5c6d48861e4f
Steve Suhr <steve.suhr@gmail.com>
2025-08-09 12:46:59 -0700

    adding Google sign in

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import '../styles/globals.css';
+import { SessionProvider } from "next-auth/react";
 import TopNav from '../components/TopNav';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps }  }) {
   return (
     <>
-      <TopNav />
-      <div className="pt-16">
-        <Component {...pageProps} />
-      </div>
+      <SessionProvider session={session}>
+
+        <TopNav />
+        <div className="pt-16">
+          <Component {...pageProps} />
+        </div>
+
+      </SessionProvider>
     </>
   );
 }
