f27a0bb8c6ccd2e1a959a60ac56c2ae778a26d05
Steve Suhr <steve.suhr@gmail.com>
2025-08-12 20:34:00 -0700

    feat: migrate app shell to app router

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -10,4 +10,6 @@ export default NextAuth({
   ],
   // optional: customize pages, callbacks, etc.
   session: { strategy: "jwt" },
-});
+};
+
+export default NextAuth(authOptions);
