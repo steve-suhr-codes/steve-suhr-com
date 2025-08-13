43f865a574c135eb2a61796ef7771cbf49b1f523
Steve Suhr <steve.suhr@gmail.com>
2025-08-12 20:54:18 -0700

    bug fix, removing session provider from server component (the layout)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
 import TopNav from '../components/TopNav';
@@ -15,30 +14,14 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
-        <SessionProvider session={session as any}>
           <TopNav isAuthed={isAuthed} />
           <div className="pt-16">{children}</div>
-        </SessionProvider>
       </body>
     </html>
   );
