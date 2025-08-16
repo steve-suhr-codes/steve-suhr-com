68cfdd814eb98e4aeae76a16390e35f0c5a4ac9c
Steve Suhr <steve.suhr@gmail.com>
2025-08-16 13:13:15 -0700

    removing unneeded files

--- app/action.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import { prisma } from "@/lib/prisma";
-
-export async function addTodo(userId: string, title: string) {
-  return; //prisma.todo.create({ data: { userId, title } });
-}
