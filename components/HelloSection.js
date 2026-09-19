50562415e0a4613cb85b5ee6de133478c549069a
Steve Suhr <steve.suhr@gmail.com>
2026-09-19 08:20:35 -0700

    new intro copy

--- components/HelloSection.js
+++ components/HelloSection.js
@@ -2,7 +2,7 @@ export default function HelloSection() {
   return (
     <div className="resume-intro">
       <p>
-        I&apos;m a professional software developer with nearly two decades of experience. Most of my expertise is in backend development with a focus in C# and MSSQL. However, I am a full stack developer with front end experience as well. I&apos;ve also been heavily involved in hosting my work in the cloud using both Azure and AWS.
+        I&apos;m Steve Suhr, a software developer with two decades of experience turning messy problems into software that&apos;s built to last. I lean backend by trade, but I&apos;m comfortable across the stack. I build for the cloud with performance, maintainability, and extensibility in mind.
       </p>
     </div>
   );
